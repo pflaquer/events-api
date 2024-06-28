3a9b45e699bd121f9d41c134e3419313084c9d9f
Dromance <88290916+pflaquer@users.noreply.github.com>
2024-06-28 14:08:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.post("/add")
+def post_Item():
+    return {"itemstatus":"posted"}
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
